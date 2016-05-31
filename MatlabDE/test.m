c876f621de702d4ee40bcb9782c45eda4b723219
Guilherme Brunow Gomes <gbrunowgomes@crimson.ua.edu>
2016-05-30 21:11:47 -0300

    SHADE added

--- MatlabDE/test.m
+++ MatlabDE/test.m
@@ -9,8 +9,8 @@ NP = 75;
 maxASize = NP;
 tic
 % feedback = @(g) progressBar(30,g,n,true,false,true,true);
-% best = SHADE(D, NP, n, -100, 100, maxASize, eval, @progress);
-best = JADE(D, NP, n, -100, 100, maxASize, eval, @progress);
+best = SHADE(D, NP, n, -100, 100, maxASize, eval, @progress);
+% best = JADE(D, NP, n, -100, 100, maxASize, eval, @progress);
 % best = DE(D, NP, n, -100, 100, eval, @progress);
 f.b = best(1:(D/2));
 f.a = best((D/2 +1):end);
